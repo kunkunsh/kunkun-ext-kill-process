1dc60af45c69f358765f8d875f6eb7eebb3a2961
Huakun Shen <huakun.shen@huakunshen.com>
2025-02-26 09:59:47 -0500

    feat: enhance process list view with detailed metadata and pretty-bytes

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import prettyBytes from "pretty-bytes";
 import {
   Action,
   expose,
@@ -8,6 +9,7 @@ import {
   toast,
   ui,
   clipboard,
+  Markdown,
 } from "@kksh/api/ui/template";
 
 class ExtensionTemplate extends TemplateUiCommand {
@@ -15,6 +17,61 @@ class ExtensionTemplate extends TemplateUiCommand {
   async onFormSubmit(value: Record<string, any>): Promise<void> {
     toast.success(`Form submitted: ${JSON.stringify(value)}`);
   }
+
+  onHighlightedListItemChanged(value: string): Promise<void> {
+    super.onHighlightedListItemChanged(value);
+    console.log("Highlighted list item changed:", value);
+    if (!this.highlightedListItemValue) {
+      return toast.warning("No process selected");
+    }
+    const { pid, name } = JSON.parse(this.highlightedListItemValue);
+    const process = this.processes.find((p) => p.pid === pid);
+    if (!process) {
+      return toast.warning("Process not found");
+    }
+    ui.render(
+      new List.List({
+        inherits: ["items"],
+        detail: new List.ItemDetail({
+          children: [
+            new List.ItemDetailMetadata([
+              new List.ItemDetailMetadataLabel({
+                title: "Name",
+                text: name,
+              }),
+              new List.ItemDetailMetadataLabel({
+                title: "PID",
+                text: pid.toString(),
+              }),
+              new List.ItemDetailMetadataLabel({
+                title: "CPU Usage",
+                text: `${(process.cpu_usage ?? 0).toFixed(2)}%`,
+              }),
+              new List.ItemDetailMetadataLabel({
+                title: "Memory Usage",
+                text: prettyBytes(process.memory ?? 0),
+              }),
+              new List.ItemDetailMetadataLabel({
+                title: "Parent",
+                text: process.parent?.toString() ?? "Unknown",
+              }),
+              new List.ItemDetailMetadataLabel({
+                title: "Group",
+                text: process.group_id?.toString() ?? "Unknown",
+              }),
+            ]),
+            new Markdown(`
+- **exe:** ${process.exe}
+- **Virtual Memory:** ${prettyBytes(process.virtual_memory ?? 0)}
+`),
+          ],
+          width: 50,
+        }),
+      })
+    );
+    return Promise.resolve();
+  }
+
   async load() {
     ui.render(new List.List({ items: [] }));
     sysInfo
@@ -22,11 +79,12 @@ class ExtensionTemplate extends TemplateUiCommand {
       .then(() => sysInfo.processes())
       .then((processes) => {
         this.processes = processes;
-        processes.sort((a, b) => (b.cpu_usage ?? 0) - (a.cpu_usage ?? 0));
-        console.log(processes);
+        this.processes.sort((a, b) => (b.cpu_usage ?? 0) - (a.cpu_usage ?? 0));
+        console.log(this.processes);
+        ui.setSearchBarPlaceholder("Search by process name or pid");
         ui.render(
           new List.List({
-            items: processes.map((p) => {
+            items: this.processes.map((p) => {
               //   const exe = p.exe ?? "Unknown Executable";
               //   const trimmedExe = exe.length > 50 ? "..." + exe.slice(-47) : exe;
               return new List.Item({
